a143a169cfc240af4896abfae1276de7df78c01e
Michal Srb <srb@convex.dev>
2024-07-01 14:48:43 +0200

    Run convex codegen
    npx convex codegen

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
 import type * as messages from "../messages.js";
 
 /**
@@ -25,6 +27,8 @@ import type * as messages from "../messages.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
+  http: typeof http;
   messages: typeof messages;
 }>;
 export declare const api: FilterApi<
