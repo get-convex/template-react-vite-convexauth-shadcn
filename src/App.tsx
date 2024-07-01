e9a982aae8d5045d8406055dba99dcc294a52676
Michal Srb <srb@convex.dev>
2024-07-01 16:16:22 +0200

    Working demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import { Chat } from "@/Chat/Chat";
 import { ChatIntro } from "@/Chat/ChatIntro";
-import { randomName } from "@/Chat/randomName";
 import { Layout } from "@/Layout";
 import { SignInForm } from "@/SignInForm";
 import { UserMenu } from "@/components/UserMenu";
-import { Authenticated, Unauthenticated } from "convex/react";
-import { useState } from "react";
+import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
 
 export default function App() {
-  const [viewer] = useState(randomName());
+  const user = useQuery(api.users.viewer);
   return (
     <Layout
       menu={
         <Authenticated>
-          <UserMenu>{viewer}</UserMenu>
+          <UserMenu>{user?.name ?? user?.email}</UserMenu>
         </Authenticated>
       }
     >
       <>
         <Authenticated>
           <ChatIntro />
-          <Chat viewer={viewer} />
+          <Chat viewer={(user ?? {})._id!} />
         </Authenticated>
         <Unauthenticated>
           <SignInForm />
