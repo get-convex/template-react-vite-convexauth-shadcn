480a411d2f6eb20413e686062e76a8ba82189a0c
Michal Srb <srb@convex.dev>
2024-07-01 16:07:30 +0200

    Added sign-in form with oauth and magic link

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,30 @@ import { Chat } from "@/Chat/Chat";
 import { ChatIntro } from "@/Chat/ChatIntro";
 import { randomName } from "@/Chat/randomName";
 import { Layout } from "@/Layout";
+import { SignInForm } from "@/SignInForm";
 import { UserMenu } from "@/components/UserMenu";
+import { Authenticated, Unauthenticated } from "convex/react";
 import { useState } from "react";
 
 export default function App() {
   const [viewer] = useState(randomName());
   return (
-    <Layout menu={<UserMenu>{viewer}</UserMenu>}>
-      <ChatIntro />
-      <Chat viewer={viewer} />
+    <Layout
+      menu={
+        <Authenticated>
+          <UserMenu>{viewer}</UserMenu>
+        </Authenticated>
+      }
+    >
+      <>
+        <Authenticated>
+          <ChatIntro />
+          <Chat viewer={viewer} />
+        </Authenticated>
+        <Unauthenticated>
+          <SignInForm />
+        </Unauthenticated>
+      </>
     </Layout>
   );
 }
